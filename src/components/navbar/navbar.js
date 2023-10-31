a06588227b35b9a328b08b8b1c0f080bb5731d3d
Lijazilla <alejandro-1986-jf@hotmail.com>
2023-10-30 22:52:42 -0600

    se modificó la referencia del titulo para que mande a home

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -4,7 +4,7 @@ function Navbar({ brand }) {
     return (
         <nav className="navbar navbar-dark bg-primary">
             <div className="container">
-                <a className="navbar-brand text-uppercase" href="/">
+                <a className="navbar-brand text-uppercase" href="https://lijazilla.github.io/apitest/build/">
                     {brand}
                 </a>
             </div>
